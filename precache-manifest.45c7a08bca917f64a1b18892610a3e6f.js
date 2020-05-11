61bb2ef724246fdfa7b89fff7c89c69ed9698e3c
Diego-si <diventura1@gmail.com>
2020-05-11 08:09:26 -0300

    Updates

---
+++ precache-manifest.45c7a08bca917f64a1b18892610a3e6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8c594fa18a0dc80b1c2cef4320dd29",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "df060e28e5dc0dfe6b0c",
+    "url": "/ReactJS/static/css/2.f1d36a8f.chunk.css"
+  },
+  {
+    "revision": "df060e28e5dc0dfe6b0c",
+    "url": "/ReactJS/static/js/2.2c05d45e.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/ReactJS/static/js/2.2c05d45e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f01cce6fa63a067a14bf",
+    "url": "/ReactJS/static/js/main.a61565e2.chunk.js"
+  },
+  {
+    "revision": "268857ef316c00cc5a3f",
+    "url": "/ReactJS/static/js/runtime-main.8fc435c6.js"
+  }
+]);
